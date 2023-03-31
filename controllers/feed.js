593574d9f1ccc06c528449e26817e2cb192edb7c
Rohank1442 <rohankumar1442@gmail.com>
2023-03-31 23:19:02 +0530

    C4

--- controllers/feed.js
+++ controllers/feed.js
@@ -22,13 +22,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        // console.log('hi2')
-        return res
-        .status(422)
-        .json({
-            message: 'Validation failed, entered data is incorrect',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrecet.');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -41,14 +37,17 @@ exports.createPost = (req, res, next) => {
     });
     post
     .save()
-    .then(result => {
-        console.log(err);
-        res.status(201).json({
-            message: 'Post created successfully!',
-            post: result
+        .then(result => {
+            console.log(err);
+            res.status(201).json({
+                message: 'Post created successfully!',
+                post: result
+            });
+        })
+        .catch(err => {
+            if(err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-};
+    };
