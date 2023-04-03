d8c34eb0bcca24b2dc907d0bdc44e7f6fc7fbe11
Rohank1442 <rohankumar1442@gmail.com>
2023-04-03 23:57:24 +0530

    Final

--- models/post.js
+++ models/post.js
@@ -15,10 +15,12 @@ const postSchema = new Schema({
         required: true
     }, 
     creator: {
-        type: Object, 
-        required: String
+        type: Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
     }
-}, { timestamps: true }
+},
+ { timestamps: true }
 );
 
 module.exports = mongoose.model('Post', postSchema);
